b85cd63aa2c3da358b090282b124b4377c23c3f0
F0cusn1k <sluparev@yandex.ru>
2024-10-01 18:28:25 +0300

    lab2 done

---
+++ lab2/Lab2/Lab2/app/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('', index),
+    path('operations/<int:operation_id>/', operation_details, name="operation_details"),
+    path('operations/<int:operation_id>/add_to_calculation/', add_operation_to_draft_calculation, name="add_operation_to_draft_calculation"),
+    path('calculations/<int:calculation_id>/delete/', delete_calculation, name="delete_calculation"),
+    path('calculations/<int:calculation_id>/', calculation)
+]
